b372c2531c1d8a4878512309730b7522efa9fc5b
aducourthial <aducourthial@advestis.fr>
2020-06-01 13:56:26 +0200

    la kpop c b1 mdr

--- test.py
+++ test.py
@@ -12,3 +12,16 @@ def oui(i: str) -> str:
     str
     """
     return i
+
+def non(i: str) -> str:
+    """
+
+    Parameters
+    ----------
+    i : str
+
+    Returns
+    -------
+    str
+    """
+    return i
