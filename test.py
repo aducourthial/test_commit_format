eb5a259b2b9bed82ea0735b1efb3fcf510c39262
aducourthial <aducourthial@advestis.fr>
2020-06-01 15:17:47 +0200

    MDRLOL LA KPOP

--- test.py
+++ test.py
@@ -25,3 +25,30 @@ def non(i: str) -> str:
     str
     """
     return i
+import numpy as np
+
+def oui(i: str) -> str:
+    """
+
+    Parameters
+    ----------
+    i : str
+
+    Returns
+    -------
+    str
+    """
+    return i
+
+def non(i: str) -> str:
+    """
+
+    Parameters
+    ----------
+    i : str
+
+    Returns
+    -------
+    str
+    """
+    return i
