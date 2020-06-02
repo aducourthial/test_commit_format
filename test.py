cbf5279aa51c0e4ac53a84257584e327241776b2
aducourthial <aducourthial@advestis.fr>
2020-06-02 10:28:40 +0200

    szqkghuhfe

--- test.py
+++ test.py
@@ -88,20 +88,6 @@ def non(i: str) -> str:
     return i
 
 
-def non(i: str) -> str:
-    """
-
-    Parameters
-    ----------
-    i : str
-
-    Returns
-    -------
-    str
-    """
-    return i
-
-
 import numpy as np
 
 
